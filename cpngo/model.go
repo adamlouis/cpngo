bacbd3424b8113812954582d5fad2beff8d35ff3
Adam Suczewski <adamlouis@users.noreply.github.com>
2023-02-18 19:21:34 -0800

    refactor

---
+++ cpngo/model.go
@@ -0,0 +1,31 @@
+package cpngo
+
+type Net struct {
+	Places      []Place      `json:"places"`
+	Transitions []Transition `json:"transitions"`
+	InputArcs   []InputArc   `json:"input_arcs"`
+	OutputArcs  []OutputArc  `json:"output_arcs"`
+	Tokens      []Token      `json:"tokens"`
+}
+
+type Place struct {
+	ID string `json:"id"`
+}
+type Transition struct {
+	ID string `json:"id"`
+}
+type InputArc struct {
+	ID     string `json:"id"`
+	FromID string `json:"from_id"`
+	ToID   string `json:"to_id"`
+}
+type OutputArc struct {
+	ID     string `json:"id"`
+	FromID string `json:"from_id"`
+	ToID   string `json:"to_id"`
+}
+type Token struct {
+	ID      string `json:"id"`
+	PlaceID string `json:"place_id"`
+	Color   any    `json:"color"`
+}
